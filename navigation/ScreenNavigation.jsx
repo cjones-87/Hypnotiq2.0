6685064b1d3fe86891dd13f50790db2cbb3233fd
CJ <cliftonjones.87@gmail.com>
2023-06-07 22:57:52 -0400

    refactor: implemented lazy loading of routes/screens with react's lazy and suspense api

--- navigation/ScreenNavigation.jsx
+++ navigation/ScreenNavigation.jsx
@@ -1,88 +1,96 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { Suspense, lazy, useEffect, useState } from 'react';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import Splash from '../screens/splash/Splash.jsx';
-import LoginScreen from '../screens/authentication/login/LoginScreen.jsx';
-import ForgotPasswordScreen from '../screens/authentication/forgottenPassword/ForgotPasswordScreen.jsx';
-import RegistrationScreen from '../screens/authentication/registration/RegistrationScreen.jsx';
-
-import HomeScreen from '../screens/home/HomeScreen';
-import SearchScreen from '../screens/search/SearchScreen';
-import LibraryScreen from '../screens/library/LibraryScreen';
-// import AudioPlaylist from '../screens/music/audioPlaylist/AudioPlaylist';
-import AudioPlayer from '../screens/music/audioPlayer/AudioPlayer';
-import AudioMenu from '../screens/music/audioMenu/AudioMenu';
-import PlaylistDetail from '../screens/library/PlaylistDetail.jsx';
-
 import { firebase } from '../firebase';
-
+import Spinner from '../misc/Spinner';
 import AudioProvider from '../context/AudioProvider.jsx';
-
 import color from '../misc/color.js';
-
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Entypo from '@expo/vector-icons/Entypo';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+const Splash = lazy(() => import('../screens/splash/Splash.jsx'));
+const LoginScreen = lazy(() =>
+  import('../screens/authentication/login/LoginScreen.jsx')
+);
+const ForgotPasswordScreen = lazy(() =>
+  import('../screens/authentication/forgottenPassword/ForgotPasswordScreen.jsx')
+);
+const RegistrationScreen = lazy(() =>
+  import('../screens/authentication/registration/RegistrationScreen.jsx')
+);
+const HomeScreen = lazy(() => import('../screens/home/HomeScreen'));
+const SearchScreen = lazy(() => import('../screens/search/SearchScreen'));
+const LibraryScreen = lazy(() => import('../screens/library/LibraryScreen'));
+const PlaylistDetail = lazy(() =>
+  import('../screens/library/PlaylistDetail.jsx')
+);
+const AudioPlayer = lazy(() =>
+  import('../screens/music/audioPlayer/AudioPlayer')
+);
+const AudioMenu = lazy(() => import('../screens/music/audioMenu/AudioMenu'));
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const PlaylistScreen = () => {
   return (
-    <Stack.Navigator
-      initialRouteName="Library Screen"
-      screenOptions={{ headerTitleAlign: 'center', headerShown: false }}
-    >
-      <Stack.Screen
-        component={LibraryScreen}
-        name="Library Screen"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        component={PlaylistDetail}
-        name="Playlist Detail"
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
+    <Suspense fallback={<Spinner />}>
+      <Stack.Navigator
+        initialRouteName="Library Screen"
+        screenOptions={{ headerTitleAlign: 'center', headerShown: false }}
+      >
+        <Stack.Screen
+          component={LibraryScreen}
+          name="Library Screen"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          component={PlaylistDetail}
+          name="Playlist Detail"
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </Suspense>
   );
 };
 
 const BottomNavigationBar = () => {
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{ headerTitleAlign: 'center', headerShown: false }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: () => (
-            <FontAwesome5 name="home" color={color.ACTIVE_BG} size={25} />
-          ),
-          tabBarActiveBackgroundColor: 'black',
-          tabBarActiveTintColor: color.FONT,
-          tabBarInactiveBackgroundColor: color.FONT,
-          tabBarInactiveTintColor: color.ACTIVE_BG,
-        }}
-      />
-      <Tab.Screen
-        name="Browse"
-        component={SearchScreen}
-        options={{
-          tabBarIcon: () => (
-            <FontAwesome5 name="search" color={color.ACTIVE_BG} size={25} />
-          ),
-          tabBarActiveBackgroundColor: 'black',
-          tabBarActiveTintColor: color.FONT,
-          tabBarInactiveBackgroundColor: color.FONT,
-          tabBarInactiveTintColor: color.ACTIVE_BG,
-        }}
-      />
-      {/* <Tab.Screen
+    <Suspense fallback={<Spinner />}>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerTitleAlign: 'center', headerShown: false }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: () => (
+              <FontAwesome5 name="home" color={color.ACTIVE_BG} size={25} />
+            ),
+            tabBarActiveBackgroundColor: 'black',
+            tabBarActiveTintColor: color.FONT,
+            tabBarInactiveBackgroundColor: color.FONT,
+            tabBarInactiveTintColor: color.ACTIVE_BG,
+          }}
+        />
+        <Tab.Screen
+          name="Browse"
+          component={SearchScreen}
+          options={{
+            tabBarIcon: () => (
+              <FontAwesome5 name="search" color={color.ACTIVE_BG} size={25} />
+            ),
+            tabBarActiveBackgroundColor: 'black',
+            tabBarActiveTintColor: color.FONT,
+            tabBarInactiveBackgroundColor: color.FONT,
+            tabBarInactiveTintColor: color.ACTIVE_BG,
+          }}
+        />
+
+        {/* <Tab.Screen
         name="Library"
         component={LibraryScreen}
         options={{
@@ -99,52 +107,54 @@ const BottomNavigationBar = () => {
           tabBarInactiveTintColor: color.ACTIVE_BG,
         }}
       /> */}
-      <Tab.Screen
-        name="Hypnotiq Player"
-        component={AudioPlayer}
-        options={{
-          tabBarIcon: () => (
-            <MaterialIcons name="headset" color={color.ACTIVE_BG} size={25} />
-          ),
-          tabBarActiveBackgroundColor: 'black',
-          tabBarActiveTintColor: color.FONT,
-          tabBarInactiveBackgroundColor: color.FONT,
-          tabBarInactiveTintColor: color.ACTIVE_BG,
-        }}
-      />
 
-      <Tab.Screen
-        name="Playlist"
-        component={PlaylistScreen}
-        options={{
-          tabBarIcon: () => (
-            <MaterialIcons
-              name="library-music"
-              color={color.ACTIVE_BG}
-              size={25}
-            />
-          ),
-          tabBarActiveBackgroundColor: 'black',
-          tabBarActiveTintColor: color.FONT,
-          tabBarInactiveBackgroundColor: color.FONT,
-          tabBarInactiveTintColor: color.ACTIVE_BG,
-        }}
-      />
+        <Tab.Screen
+          name="Hypnotiq Player"
+          component={AudioPlayer}
+          options={{
+            tabBarIcon: () => (
+              <MaterialIcons name="headset" color={color.ACTIVE_BG} size={25} />
+            ),
+            tabBarActiveBackgroundColor: 'black',
+            tabBarActiveTintColor: color.FONT,
+            tabBarInactiveBackgroundColor: color.FONT,
+            tabBarInactiveTintColor: color.ACTIVE_BG,
+          }}
+        />
 
-      <Tab.Screen
-        name="Library"
-        component={AudioMenu}
-        options={{
-          tabBarIcon: () => (
-            <Entypo name="folder-music" color={color.ACTIVE_BG} size={25} />
-          ),
-          tabBarActiveBackgroundColor: 'black',
-          tabBarActiveTintColor: color.FONT,
-          tabBarInactiveBackgroundColor: color.FONT,
-          tabBarInactiveTintColor: color.ACTIVE_BG,
-        }}
-      />
-    </Tab.Navigator>
+        <Tab.Screen
+          name="Playlist"
+          component={PlaylistScreen}
+          options={{
+            tabBarIcon: () => (
+              <MaterialIcons
+                name="library-music"
+                color={color.ACTIVE_BG}
+                size={25}
+              />
+            ),
+            tabBarActiveBackgroundColor: 'black',
+            tabBarActiveTintColor: color.FONT,
+            tabBarInactiveBackgroundColor: color.FONT,
+            tabBarInactiveTintColor: color.ACTIVE_BG,
+          }}
+        />
+
+        <Tab.Screen
+          name="Library"
+          component={AudioMenu}
+          options={{
+            tabBarIcon: () => (
+              <Entypo name="folder-music" color={color.ACTIVE_BG} size={25} />
+            ),
+            tabBarActiveBackgroundColor: 'black',
+            tabBarActiveTintColor: color.FONT,
+            tabBarInactiveBackgroundColor: color.FONT,
+            tabBarInactiveTintColor: color.ACTIVE_BG,
+          }}
+        />
+      </Tab.Navigator>
+    </Suspense>
   );
 };
 
@@ -176,56 +186,60 @@ const Routes = () => {
 
   if (!user) {
     return (
-      <Stack.Navigator
-        initialRouteName="Splash Screen"
-        screenOptions={{ headerTitleAlign: 'center', headerShown: false }}
-      >
-        <Stack.Screen
-          component={Splash}
-          name="Splash Screen"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          component={LoginScreen}
-          name="Login Screen"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          component={ForgotPasswordScreen}
-          name="Forgot Password Screen"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          component={RegistrationScreen}
-          name="Registration Screen"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          component={BottomNavigationBar}
-          name="Bottom Navigation Bar"
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      <Suspense fallback={<Spinner />}>
+        <Stack.Navigator
+          initialRouteName="Splash Screen"
+          screenOptions={{ headerTitleAlign: 'center', headerShown: false }}
+        >
+          <Stack.Screen
+            component={Splash}
+            name="Splash Screen"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            component={LoginScreen}
+            name="Login Screen"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            component={ForgotPasswordScreen}
+            name="Forgot Password Screen"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            component={RegistrationScreen}
+            name="Registration Screen"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            component={BottomNavigationBar}
+            name="Bottom Navigation Bar"
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </Suspense>
     );
   }
 
   return (
-    <Stack.Navigator
-      initialRouteName="Splash Screen"
-      screenOptions={{ headerTitleAlign: 'center', headerShown: false }}
-    >
-      <Stack.Screen name="Splash Screen" component={Splash} />
-      <Stack.Screen
-        name="Bottom Navigation Bar"
-        component={BottomNavigationBar}
-        options={{ headerShown: false }}
-      />
-      {/* <Stack.Screen
+    <Suspense fallback={<Spinner />}>
+      <Stack.Navigator
+        initialRouteName="Splash Screen"
+        screenOptions={{ headerTitleAlign: 'center', headerShown: false }}
+      >
+        <Stack.Screen name="Splash Screen" component={Splash} />
+        <Stack.Screen
+          name="Bottom Navigation Bar"
+          component={BottomNavigationBar}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen
         component={PlaylistDetail}
         name="Playlist Detail"
         options={{ headerShown: false }}
       /> */}
-    </Stack.Navigator>
+      </Stack.Navigator>
+    </Suspense>
   );
 };
 
